bf43fe8c704cbabe411cdbdf4e221feb102ce25b
AustinMusuya <amusuya57@gmail.com>
2025-08-18 12:14:11 +0300

    refactor : project folder structure to match checker requirments

---
+++ graphql_crm/schema.py
@@ -0,0 +1,14 @@
+import graphene
+from graphene_django import DjangoObjectType
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hello, GraphQL!")
+
+
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- schema.py
+++ schema.py
@@ -1,14 +1,2 @@
-import graphene
-from graphene_django import DjangoObjectType
-from crm.schema import Query as CRMQuery, Mutation as CRMMutation
-
-
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
-
-
-class Mutation(CRMMutation, graphene.ObjectType):
-    pass
-
-
-schema = graphene.Schema(query=Query, mutation=Mutation)
+# schema.py (root) - re-export the main schema
+from graphql_crm.schema import schema
